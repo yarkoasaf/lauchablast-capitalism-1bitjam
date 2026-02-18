b8b7391852718ba1085d0e1a70393467fe8e341a
Yarko Bahamonde <yarko.bahamonde@usm.cl>
2026-02-18 17:20:22 -0300

    colition and arrow rotation fix

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -7,20 +7,22 @@ extends CharacterBody2D
 var going_up := false
 var area : bool = 1
 
+
 func _physics_process(_delta: float) -> void:
 	# Toggle on key press
 	if Input.is_action_just_pressed("wave"):
 		going_up = !going_up
+		rotation = 0.75 if velocity.y < 0 else -0.75
 
 	velocity.y = -speed_y if going_up else speed_y
 	velocity.x = speed_x
-	rotation = -340 if going_up else 340
+	
 	move_and_slide()
 	
 	#gain money if area top, lose money if area bottom
-	global.gain_money(1) if area else global.lose_money(1)
+	global.gain_money(2) if area else global.lose_money(2)
 	#gain money if go up, lose money if go down
-	global.gain_money(2) if velocity.y < 0 else global.lose_money(2)
+	global.gain_money(1) if velocity.y < 0 else global.lose_money(1)
 	
 	
 
