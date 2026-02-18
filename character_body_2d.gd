665d511b427aaecf897a9de78373a428770da2d8
Yarko Bahamonde <yarko.bahamonde@usm.cl>
2026-02-18 16:28:55 -0300

    feat: money gain/loose trigger

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,8 +2,10 @@
 extends CharacterBody2D
 
 @export var speed_y := 300.0
+@export var speed_x := 10.0
 
 var going_up := false
+var area : bool = 1
 
 func _physics_process(_delta: float) -> void:
 	# Toggle on key press
@@ -11,7 +13,31 @@ func _physics_process(_delta: float) -> void:
 		going_up = !going_up
 
 	velocity.y = -speed_y if going_up else speed_y
+	velocity.x = speed_x
 	rotation = -340 if going_up else 340
 	move_and_slide()
 	
-	global.gain_money(1) if velocity.y < 0 else global.lose_money(1)
+	#gain money if area top, lose money if area bottom
+	global.gain_money(1) if area else global.lose_money(1)
+	#gain money if go up, lose money if go down
+	global.gain_money(2) if velocity.y < 0 else global.lose_money(2)
+	
+	
+
+
+#enter
+func _on_area_lose_body_entered(body: Node2D) -> void:
+	area = 0
+	print("lose enter area")
+
+func _on_area_gain_body_entered(body: Node2D) -> void:
+	area = 1
+	print("gain enter area")
+
+#exit
+func _on_area_gain_body_exited(body: Node2D) -> void:
+	print("gain exit area")
+
+
+func _on_area_lose_body_exited(body: Node2D) -> void:
+	print("lose exit area")
